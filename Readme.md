26be90aee34258fd369cc14e813b1a182525c37f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# How to place one circular scale into another
+
+
+<p>In some cases you may need to place several circular scales into the main one.  The following example demonstrates how to create a typical watch with several scales (the main scale will show the current time, while the minor scales will show  current day, week and month).</p><p><br />
+</p>
+
+
+<h3>Description</h3>
+
+<p>To place several scales inside the main one, it&#39;s necessary to create an appropriate <a href="http://msdn.microsoft.com/ru-ru/library/system.windows.controls.itemspaneltemplate.aspx"><u>ItemsPanelTemplate</u></a> object and assign it to the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfGaugesGaugeControlBase_ScalePanelTemplatetopic"><u>ScalePanelTemplate</u></a> property. In this example we will use the <strong>Grid </strong>panel with <strong>2</strong> rows and <strong>4</strong> columns as a custom scale panel. </p><p>After defining a custom scale panel&#39;s template, it&#39;s required to set the <a href="http://msdn.microsoft.com/ru-ru/library/system.windows.controls.grid.rowspan.aspx"><u>Grid.RowSpan</u></a> and <a href="http://msdn.microsoft.com/ru-ru/library/system.windows.controls.grid.columnspan.aspx"><u>Grid.ColumnSpan</u></a> attached properties to<strong> 2</strong> and <strong>4</strong> for the main scale, so that it occupies the entire grid area. For other circular scales, <a href="http://msdn.microsoft.com/en-us/library/cc917861(VS.95).aspx"><u>Grid.Column</u></a> and <a href="http://msdn.microsoft.com/en-us/library/cc917855(v=VS.96).aspx"><u>Grid.Row</u></a>  properties should be used to define their positions inside the grid area (and therefore, inside the main scale).</p><p><br />
+<strong>Note</strong>: the declaration of the main scale in XAML is placed after declarations of all minor scales. This is done in order to paint needles of the main scale above all spindle caps of minor scales.</p><p><br />
+</p>
+
+<br/>
+
+
---
+++ VB/ScaleLayoutSample/MainPage.xaml.vb
@@ -0,0 +1,109 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Threading
+
+Namespace ScaleLayoutSample
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			  DataContext = New ClockViewModel()
+		End Sub
+	End Class
+
+	Public Class ClockViewModel
+		Inherits DependencyObject
+		Public Shared ReadOnly HourProperty As DependencyProperty = DependencyProperty.Register("Hour", GetType(Double), GetType(ClockViewModel), New PropertyMetadata(0.0))
+		Public Shared ReadOnly MinuteProperty As DependencyProperty = DependencyProperty.Register("Minute", GetType(Double), GetType(ClockViewModel), New PropertyMetadata(0.0))
+		Public Shared ReadOnly SecondProperty As DependencyProperty = DependencyProperty.Register("Second", GetType(Double), GetType(ClockViewModel), New PropertyMetadata(0.0))
+		Public Shared ReadOnly DayOfWeekProperty As DependencyProperty = DependencyProperty.Register("DayOfWeek", GetType(Double), GetType(ClockViewModel), New PropertyMetadata(0.0))
+		Public Shared ReadOnly DayOfMonthProperty As DependencyProperty = DependencyProperty.Register("DayOfMonth", GetType(Double), GetType(ClockViewModel), New PropertyMetadata(0.0))
+		Public Shared ReadOnly MonthOfYearProperty As DependencyProperty = DependencyProperty.Register("MonthOfYear", GetType(Double), GetType(ClockViewModel), New PropertyMetadata(0.0))
+
+		Public Property Hour() As Double
+			Get
+				Return CDbl(GetValue(HourProperty))
+			End Get
+			Set(ByVal value As Double)
+				SetValue(HourProperty, value)
+			End Set
+		End Property
+
+		Public Property Minute() As Double
+			Get
+				Return CDbl(GetValue(MinuteProperty))
+			End Get
+			Set(ByVal value As Double)
+				SetValue(MinuteProperty, value)
+			End Set
+		End Property
+
+		Public Property Second() As Double
+			Get
+				Return CDbl(GetValue(SecondProperty))
+			End Get
+			Set(ByVal value As Double)
+				SetValue(SecondProperty, value)
+			End Set
+		End Property
+
+		Public Property DayOfWeek() As Double
+			Get
+				Return CDbl(GetValue(DayOfWeekProperty))
+			End Get
+			Set(ByVal value As Double)
+				SetValue(DayOfWeekProperty, value)
+			End Set
+		End Property
+
+		Public Property DayOfMonth() As Double
+			Get
+				Return CInt(Fix(GetValue(DayOfMonthProperty)))
+			End Get
+			Set(ByVal value As Double)
+				SetValue(DayOfMonthProperty, value)
+			End Set
+		End Property
+
+		Public Property MonthOfYear() As Double
+			Get
+				Return CDbl(GetValue(MonthOfYearProperty))
+			End Get
+			Set(ByVal value As Double)
+				SetValue(MonthOfYearProperty, value)
+			End Set
+		End Property
+
+		Private ReadOnly timer As New DispatcherTimer()
+
+		Public Sub New()
+			AddHandler timer.Tick, AddressOf OnTimedEvent
+			timer.Start()
+			Update()
+		End Sub
+
+		Private Sub Update()
+			Dim currentDate As DateTime = DateTime.Now
+
+			Second = (currentDate.Second / 60.0) * 12
+			Minute = ((currentDate.Minute + currentDate.Second / 60.0) / 60.0) * 12
+			Hour = (currentDate.Hour Mod 12) + currentDate.Minute / 60.0
+
+			DayOfMonth = currentDate.Day
+			DayOfWeek = CInt(Fix(currentDate.DayOfWeek))
+			MonthOfYear = currentDate.Month
+		End Sub
+
+		Private Sub OnTimedEvent(ByVal source As Object, ByVal e As EventArgs)
+			Update()
+		End Sub
+	End Class
+End Namespace
+
+
+
+
+
